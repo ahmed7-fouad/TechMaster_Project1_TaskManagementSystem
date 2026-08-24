c7797d7cf82262fd305721a571fd22f7279f97cd
Ahmed Fouad Abodeaf <100656655+ahmed7-fouad@users.noreply.github.com>
2026-08-24 22:08:11 +0300

    Edit Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
 import SideBar from './shared/SideBar'
+import Nav from './shared/Nav';
+import { Plus } from "lucide-react";
+import MainBtn from './shared/MainBtn';
 function App() {
   return (
-    <>
-     <SideBar/>
-    </>
-  )
+    <section className="grid grid-cols-[21rem_1fr]">
+      <SideBar />
+      <section>
+        <Nav
+          btnColor="var(--secondaryc)"
+          btnContentColor="whitesmoke"
+          icon={<Plus />}
+          title="tasks"
+          desc="manage and organize your tasks"
+          btnContent="add task"
+        />
+      </section>
+    </section>
+  );
 }
 
 export default App

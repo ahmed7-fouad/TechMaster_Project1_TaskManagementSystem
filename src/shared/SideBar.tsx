901a88138c2df476e134590478f66f5b33ceb9aa
Ahmed Fouad Abodeaf <100656655+ahmed7-fouad@users.noreply.github.com>
2026-08-26 22:53:44 +0300

    refactor: Clean up SideBar component by removing unused code

--- src/shared/SideBar.tsx
+++ src/shared/SideBar.tsx
@@ -116,36 +116,5 @@ const SideBar = ({
   );
 };
 
-const SideBar = () => {
-  const location = useLocation();
-
-  return (
-    <aside className="py-4 px-5 bg-primaryc h-screen sticky top-0">
-      <section className="w-full bg-white rounded-xl flex items-center justify-center mb-7 p-2">
-        <img src="/src/assets/logo.png" alt="LogoIcon" className="w-32" />
-      </section>
-      <ul className="space-y-2">
-        {dashboardBtns.map((btn) => {
-          const isActive = location.pathname === btn.path;
-
-          return (
-            <li key={btn.id}>
-              <Link
-                to={btn.path}
-                className={cn(
-                  "hover:bg-secondaryc flex items-center gap-3 w-full rounded-lg p-4 cursor-pointer duration-250 text-muted-text text-xl",
-                  isActive && "bg-secondaryc text-indigo-200 font-semibold",
-                )}
-              >
-                {btn.icon}
-                <h3 className="capitalize">{btn.title}</h3>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </aside>
-  );
-};
 
 export default SideBar;

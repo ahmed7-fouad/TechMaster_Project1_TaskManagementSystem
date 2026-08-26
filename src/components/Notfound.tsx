b3700121574f43960229d597339438c178835106
Ahmed Fouad Abodeaf <100656655+ahmed7-fouad@users.noreply.github.com>
2026-08-26 23:56:31 +0300

    feat: enhance Notfound component with 404 message and navigation link

--- src/components/Notfound.tsx
+++ src/components/Notfound.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Notfound() {
   return (
-    <div>
-      not found page
-    </div>
-  )
+    <section
+      className="p-5 flex flex-col items-center  h-screen"
+      style={{background:"radial-gradient(circle at 50% 30%, #2e1065 0%, #09090b 70%)"}}>
+      <h1 className="text-[23rem] font-extrabold text-secondaryc">404</h1>
+      <p className="uppercase text-muted-text text-white text-2xl mb-9">
+        Opppps This Page is not found
+      </p>
+      <Link
+        to="/"
+        className="py-3 px-7 bg-secondaryc font-bold text-white text-xl capitalize rounded-full hover:bg-thirdc duration-150"
+      >
+        back to home
+      </Link>
+    </section>
+  );
 }
